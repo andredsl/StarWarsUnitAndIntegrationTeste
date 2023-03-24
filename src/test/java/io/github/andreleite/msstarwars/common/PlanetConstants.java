4be77ccb407ebc0fff09b88442bf8bd0bd063c0b
andredsl <andredsleite@outlook.com>
2023-03-23 21:36:06 -0300

    Aula 16 - criando teste do getById

--- src/test/java/io/github/andreleite/msstarwars/common/PlanetConstants.java
+++ src/test/java/io/github/andreleite/msstarwars/common/PlanetConstants.java
@@ -5,4 +5,5 @@ import io.github.andreleite.msstarwars.domain.Planet;
 public class PlanetConstants {
 
     public static final Planet PLANET = new Planet("name", "climate", "terrain");
+    public static final Planet INVALID_PLANET = new Planet("", "", "");
 }
--- src/test/java/io/github/andreleite/msstarwars/domain/PlanetServiceTest.java
+++ src/test/java/io/github/andreleite/msstarwars/domain/PlanetServiceTest.java
@@ -1,6 +1,8 @@
 package io.github.andreleite.msstarwars.domain;
 
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import io.github.andreleite.msstarwars.repository.PlanetRepository;
 import org.junit.jupiter.api.Test;
@@ -9,7 +11,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static io.github.andreleite.msstarwars.common.PlanetConstants.PLANET;
+import static io.github.andreleite.msstarwars.common.PlanetConstants.INVALID_PLANET;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -20,12 +26,47 @@ public class PlanetServiceTest {
     @Mock
     private PlanetRepository planetRepository;
 
+    //padrão para escrita do nome do metodo operacao_estado_retorno
     @Test
     public void createPlanet_WithValidData_ReturnsPlanet(){
+        //AAA
+        //Arrange - arrumar os dados de teste
         when(planetRepository.save(PLANET)).thenReturn(PLANET);
-
+        //Act -
+        //sut = System Under Test
         Planet sut = planetService.create(PLANET);
-
+        //Assert
         assertThat(sut).isEqualTo(PLANET);
     }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException(){
+        when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+
+        assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+
+    }
+
+    @Test
+    public void getPlanet_ByExistingId_ReturnsPlanet(){
+
+        when(planetRepository.findById(anyLong())).thenReturn(Optional.of(PLANET));
+
+        Optional<Planet> sut = planetService.get(1L);
+
+        assertThat(sut).isNotEmpty();
+        assertThat(sut.get()).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void getPlanet_ByUnexistingId_ReturnsEmpty(){
+
+        when(planetRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        Optional<Planet> sut = planetService.get(1L);
+
+        assertThat(sut).isEmpty();
+    }
+
+
 }
